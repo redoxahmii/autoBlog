bbd2377132c605b7e7c149d04cde96e1bbc962e6
Redoxahmii <fullmetal2334@gmail.com>
2024-04-11 10:53:43 +0500

    adsense add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="cupcake">
+      <Script
+        async
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4263655027113293"
+      />
       <link rel="icon" href="/icon.svg" sizes="any" />
       <body className={inter.className}>
         <Navbar />
